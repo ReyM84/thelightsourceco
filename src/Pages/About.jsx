dbd22d29af929fedbfee22a255867887dc8e2115
ReyM84 <reymarquez25@gmail.com>
2021-07-02 17:58:26 -0700

    Hero component in use

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Components/Header";
 import { Box } from "@chakra-ui/layout";
+import Hero from "../Components/Hero";
 
 const About = () => {
   return (
     <Box>
+      <Hero />
       <Header color="red" />
     </Box>
   );
